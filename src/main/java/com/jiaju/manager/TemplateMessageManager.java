aef03b09f012da2d0ec04c9a6aa28c80d0df8d1c
952407502 <952407502@qq.com>
2020-06-29 00:00:43 +0800

    微信通知

--- src/main/java/com/jiaju/manager/TemplateMessageManager.java
+++ src/main/java/com/jiaju/manager/TemplateMessageManager.java
@@ -35,4 +35,40 @@ public class TemplateMessageManager {
         System.out.println(s);
 
     }
+
+
+    @Test
+    public void sendTemplateMessage() {
+        String at = WxService.getAccessToken();
+        String url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token="+at;
+        String data="{\n" +
+                "           \"touser\":\"oIK8-uF8Yq_exzn4MkV3kgc63j7c\",\n" +
+                "           \"template_id\":\"lk2aT0SdajYeVUALItKQNLWaguHYJdac-Dynqr6YI8A\",         \n" +
+                "           \"data\":{\n" +
+                "                   \"first\": {\n" +
+                "                       \"value\":\"您有新的反馈信息啦！\",\n" +
+                "                       \"color\":\"#abcdef\"\n" +
+                "                   },\n" +
+                "                   \"company\":{\n" +
+                "                       \"value\":\"哈哈公司\",\n" +
+                "                       \"color\":\"#fff000\"\n" +
+                "                   },\n" +
+                "                   \"time\": {\n" +
+                "                       \"value\":\"2020年11月11日\",\n" +
+                "                       \"color\":\"#1f1f1f\"\n" +
+                "                   },\n" +
+                "                   \"result\": {\n" +
+                "                       \"value\":\"面试通过\",\n" +
+                "                       \"color\":\"#173177\"\n" +
+                "                   },\n" +
+                "                   \"remark\":{\n" +
+                "                       \"value\":\"请和本公司人事专员联系！\",\n" +
+                "                       \"color\":\"#173177\"\n" +
+                "                   }\n" +
+                "           }\n" +
+                "       }";
+        String result = RobotUtil.post(url, data);
+        System.out.println(result);
+    }
+
 }
